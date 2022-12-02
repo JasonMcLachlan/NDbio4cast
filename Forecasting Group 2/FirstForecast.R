b255b1d22edda40d97fd249ecedd2c0a56c1904d
Ian <Shuman95100@gmail.com>
2022-12-02 18:38:18 -0500

    Added manar's edits from today's meeting. We still need to work on integrating the new NOAA data into our model, but first we need to fix the missing data

--- Forecasting Group 2/FirstForecast.R
+++ Forecasting Group 2/FirstForecast.R
@@ -9,6 +9,8 @@ library(daymetr)
 library(padr)
 devtools::install_github("EcoForecast/ecoforecastR",force=TRUE)
 
+source("~/WaterTemperaturePrediction.R")
+
 #load target data
 target <- readr::read_csv("https://data.ecoforecast.org/neon4cast-targets/aquatics/aquatics-targets.csv.gz") |>
   na.omit()
@@ -21,6 +23,7 @@ target_barc <- pad(target_barc)
 target_barc_do <- subset(target_barc, variable == "oxygen")
 target_barc_do <- pad(target_barc_do)
 target_barc_temp <- subset(target_barc, variable == "temperature")
+target_barc_temp <- subset(target_barc_temp, select = -c(site_id, variable))
 target_barc_temp <- pad(target_barc_temp)
 target_barc_chla <- subset(target_barc, variable == "chla")
 target_barc_chla <- pad(target_barc_chla)
@@ -31,32 +34,49 @@ time <- append(time, tail(time, 1) + c(1:30))
 y <- as.vector(target_barc_do$observation)
 y <- append(y, rep(NA,30))
 
-#Random Walk Timeseries Model
-RandomWalk = "
+#appending forecasted water temp
+target_barc_temp <- rbind(target_barc_temp, temp_forecast)
+
+jags_df <- merge(x = target_barc_temp, y = target_barc_do, by = "datetime", all.x = TRUE)
+colnames(jags_df) <- c("datetime", "watertemp", "site_id", "variable", "DO")
+
+c <- jags_df$watertemp
+y <- jags_df$DO
+
+#Regression Model (Water Temp ~ DO)
+WaterTempDO = "
 model{
-  
+
+  b ~ dmnorm(b0,Vb)     ## multivariate Normal prior on vector of regression params
+  S ~ dgamma(s1,s2)    ## prior precision
+  tau_obs ~ dgamma(a_obs,r_obs)
+
   #### Data Model
   for(t in 1:n){
     y[t] ~ dnorm(x[t],tau_obs)
   }
-  
+
   #### Process Model
-  for(t in 2:n){
-    x[t]~dnorm(x[t-1],tau_add)
+  for(i in 1:n){
+      mu[i] <- b[1] + b[2]*c[i]                ## process model - c1 = temp
+      x[i] ~ dnorm(mu[i],S)                		 ## data model - y = DO
   }
-  
-  #### Priors
-  x[1] ~ dnorm(x_ic,tau_ic)
-  tau_obs ~ dgamma(a_obs,r_obs)
-  tau_add ~ dgamma(a_add,r_add)
 }
 "
 #Define the data
-data <- list(y=y,n=length(y),      ## data (what do I put??)
-             x_ic=7,tau_ic=3,      ## initial condition prior (what do I put??)
-             a_obs=1,r_obs=1,      ## obs error prior (what do I put??)
-             a_add=1,r_add=1       ## process error prior (what do I put??)
-)
+# data <- list(y=y,n=length(y),      ## data (what do I put??)
+#              x_ic=7,tau_ic=3,      ## initial condition prior (what do I put??)
+#              a_obs=1,r_obs=1,      ## obs error prior (what do I put??)
+#              a_add=1,r_add=1       ## process error prior (what do I put??)
+# )
+
+data <- list(c = c, y = y, a_obs=1, r_obs=1, n = length(y))
+
+## specify priors
+data$b0 <- as.vector(c(0,0))      ## regression b means
+data$Vb <- solve(diag(10000,2))   ## regression b precisions
+data$s1 <- 0.1                    ## error prior n/2
+data$s2 <- 0.1                    ## error prior SS/2
 
 #Define MCMC Chains -- 
 nchain = 3
@@ -67,7 +87,7 @@ for(i in 1:nchain){
                     tau_obs=5/var(log(y.samp)))        ## initial guess on obs precision
 }
 
-j.model   <- jags.model (file = textConnection(RandomWalk),
+j.model   <- jags.model (file = textConnection(WaterTempDO),
                          data = data,
                          inits = init,
                          n.chains = 3)
